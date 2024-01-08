d3f7e8f8e552cfe4cca69cad349852ddc72c2006
renukanusha <renukanusha1026@gmail.com>
2024-01-09 00:21:19 +0530

    Today Apps

--- helloworldapp/src/App.js
+++ helloworldapp/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [show, toggleShow]=useState(true);
+  function myfun()
+  {
+    alert('hello');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+          <button onClick={() => toggleShow(!show)}>{show ? "HideMe" : "ShowMe"}</button>
+          {show && <h2> Hi, I'm Renuka</h2>}
+          <button onClick={myfun} color='white'>Click Me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
